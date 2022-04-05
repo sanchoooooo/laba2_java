dfe161fc07b7b225871e8d9a8ff520065f418a22
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-05 19:05:37 +0300

    Update Rec5.java

--- src/Example_5/Rec5.java
+++ src/Example_5/Rec5.java
@@ -1,2 +1,17 @@
-package Example_5;public class Rec5 {
+package Example_5;
+
+public class Rec5 {
+    public static int f(int n) {
+        if (n==0) { //повернення значення змінної при 0
+            return 0;
+        }
+        else if (n==1) {//повернення значення змінної при 1
+            return 1;
+        }
+        else return f(n-2)+f(n-1); //повернення значення змінної в інших випадках
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Fibonachi = " + f(20)); //вивід інформації, виклик f
+    }
 }
