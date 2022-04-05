974acc043c4d295c0f3c611c5421b6a85183de82
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-05 19:05:14 +0300

    Update Rec4.java

--- src/Example_4/Rec4.java
+++ src/Example_4/Rec4.java
@@ -1,2 +1,17 @@
-package Example_4;public class Rec4 {
+package Example_4;
+
+public class Rec4 {
+    public static int fact(int n) {
+        int result; //об'явлення змінної
+        if (n==1)
+            return 1; //при значенні змінної 1-повертається результат 1
+        else { //в іншому випадку виконується обчислення
+            result=fact(n-1)*n;
+            return result; //повернення результату
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Factorial = " + fact(3)); //вивід інформації, виклик fact
+    }
 }
+
