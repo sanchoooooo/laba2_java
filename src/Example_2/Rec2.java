012f8182eb8d79dbae9f59cb7ee683492b6d74d6
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-05 19:03:50 +0300

    Update Rec2.java

--- src/Example_2/Rec2.java
+++ src/Example_2/Rec2.java
@@ -1,2 +1,14 @@
-package Example_2;public class Rec2 {
+package Example_2;
+
+public class Rec2 {
+    public static void m(int x) { //функція
+        if ((2*x+1)<20){ //обчислюється результат, поки буде менше 20
+            m(2*x+1); //обчислюємий вираз
+        }
+        System.out.println("x = " + x); //вивід інформації
+    }
+    public static void main(String[] args) {
+        m(1); //виклик функції з вхідним значенням 1
+    }
 }
+
