868c6b1f77a0cb80052df6d9a8473688b9d7254b
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-05 19:04:50 +0300

    Update Rec3.java

--- src/Example_3/Rec3.java
+++ src/Example_3/Rec3.java
@@ -1,2 +1,28 @@
-package Example_3;public class Rec3 {
+package Example_3;
+
+public class Rec3 {
+    private static int step=0; //присвоєння змінній 0
+
+    public static void m(int x) {
+        space(); //виклик space
+        System.out.println(""+x+"->"); //вивід інформації
+        step++; //збільшення значення змінної на 1
+        if ((2*x+1)<20) { //обчислюється результат, поки буде менше 20
+            m(2*x+1); //обчислюємий вираз
+        }
+        step--; //зменшення значення змінної на 1
+        space(); //виклик space
+        System.out.println(""+x+"<-"); //вивід інформації
+    }
+
+    public static void space() {
+        for (int i=0; i<step; i++) {
+            System.out.print(" "); //кількість відступів=значенню step
+        }
+    }
+
+    public static void main(String[] args) {
+        m(1); //виклик m
+    }
 }
+
